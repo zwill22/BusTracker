ab2ab86e34765401747411e94e3c53afde9f4cb7
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-11-08 09:49:19 +0000

    Initial Commit

---
+++ BusTracker/BusTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTrackerApp.swift
+//  BusTracker
+//
+//  Created by Zack Williams on 08-11-2024.
+//
+
+import SwiftUI
+
+@main
+struct BusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusTrackerTests/BusTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusTrackerTests.swift
+//  BusTrackerTests
+//
+//  Created by Zack Williams on 08-11-2024.
+//
+
+import Testing
+@testable import BusTracker
+
+struct BusTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
