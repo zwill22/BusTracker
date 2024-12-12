fd045324a90ce05c07a0f174abc641a9a47a92a3
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-12-12 17:38:25 +0000

    Added OpenID

---
+++ BusTracker/Models/CLibOpenID.h
@@ -0,0 +1,8 @@
+//
+//  CLibOpenID.h
+//  BusTracker
+//
+//  Created by Zack Williams on 08-12-2024.
+//
+
+#import "OpenID.h"
---
+++ BusTracker/Models/IDProvider.swift
@@ -0,0 +1,12 @@
+//
+//  IDProvider.swift
+//  BusTracker
+//
+//  Created by Zack Williams on 08-12-2024.
+//
+
+import Foundation
+
+class IDProvider {
+    private let client = initialiseOpenIDClient();
+}
